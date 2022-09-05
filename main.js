2094fdca5ca3f3e9d0f7cbd578b0d078e10f9d42
Ayan Banerjee <ayanbanerjee102@gmail.com>
2022-09-05 17:09:35 +0530

    Completed the filter functionality

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 const form = document.querySelector("form");
 const channels = document.querySelector(".channels");
+const categories = document.querySelector(".lists");
 const ul = document.querySelector(".todos");
 
 let formValues = {};
 
 let state = {
+  channelActive: false,
+  categoryActive: false,
   selectedChannel: "",
   selectedCategory: "",
   todos: [],
 };
+
+// Functions
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -24,33 +29,66 @@ function getFormValues(e) {
 }
 
 function showUI() {
-  if (state.todos.length) {
-    let str = state.todos.reduce((acc, curr) => {
-      return (
-        acc +
-        `
+  let filteredArr = filterChannelAndCategories();
+
+  let str = filteredArr.reduce((acc, curr) => {
+    return (
+      acc +
+      `
                 <li>${curr.title}<span>${curr.category}</span><span>${curr.channel}</span></li>
                 `
-      );
-    }, "");
+    );
+  }, "");
 
-    ul.innerHTML = str;
-  } else {
-    ul.innerHTML = "";
+  ul.innerHTML = str;
+}
+
+function filterChannelAndCategories() {
+  if (state.selectedChannel === "" && state.selectedCategory === "") {
+    console.log(1);
+    return state.todos;
+  }
+
+  if (state.selectedChannel !== "" && state.selectedCategory !== "") {
+    let arr1 = state.todos.filter(
+      (item, idx) => item.channel === state.selectedChannel
+    );
+    let arr2 = arr1.filter(
+      (item, idx) => item.category === state.selectedCategory
+    );
+    console.log(2);
+    return arr2;
+  }
+
+  if (state.selectedChannel === "" && state.selectedCategory !== "") {
+    let arr = state.todos.filter(
+      (item, idx) => item.category === state.selectedCategory
+    );
+    console.log(3);
+    return arr;
+  }
+
+  if (state.selectedChannel !== "" && state.selectedCategory === "") {
+    let arr = state.todos.filter(
+      (item, idx) => item.channel === state.selectedChannel
+    );
+    console.log(4);
+    return arr;
   }
 }
 
-function sortTodos(channel) {
-  console.log(channel);
+function clickToSort(e) {
+  if (e.target.classList.contains("channel")) {
+  } else if (e.target.className === "list") {
+  }
+  showUI();
 }
 
-function selectChannels(e) {
-  let { selectedChannel } = state;
-  selectedChannel = e.target.textContent;
-  sortTodos(selectedChannel);
-}
+// state.selectedChannel = e.target.textContent;
+// state.selectedCategory = e.target.textContent.toLowerCase();
 
 // Add EventListiners
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("change", getFormValues);
-channels.addEventListener("click", selectChannels);
+channels.addEventListener("click", clickToSort);
+categories.addEventListener("click", clickToSort);
